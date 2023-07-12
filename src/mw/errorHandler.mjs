ce95b308b604ac7c9ed6b167f7dd7315f9929b68
hohlokit <kolik2001@gmail.com>
2023-07-12 15:58:01 +0300

    fix

--- src/mw/errorHandler.mjs
+++ src/mw/errorHandler.mjs
@@ -5,5 +5,5 @@ export default (err, req, res, _) => {
   console.log(`Path: ${req.path}`)
   console.log('\x1b[31m%s', String('-').repeat(50))
 
-  res.status(err?.status || 500).json({ ...err })
+  res.status(err?.status || 500).json({ message: err.message, path: err.path })
 }
