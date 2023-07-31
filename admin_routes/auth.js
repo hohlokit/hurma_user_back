0481637168e39a8271884dcaf7b6cf5fb1e5e3f8
hohlokit <kolik2001@gmail.com>
2023-07-31 13:36:19 +0300

    fix

--- admin_routes/auth.js
+++ admin_routes/auth.js
@@ -1,10 +1,9 @@
 import express from 'express'
 
 import { loginViaPassowrd } from '../controllers/auth.js'
-import { verifyToken } from '../mw/verify-token.js'
 
 const router = express.Router()
 
-router.post('/login', verifyToken, loginViaPassowrd)
+router.post('/login', loginViaPassowrd)
 
 export default router
