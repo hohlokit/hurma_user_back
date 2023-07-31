516987fc86063bfa61f011b6ef67f4a41ae7e624
hohlokit <kolik2001@gmail.com>
2023-07-31 12:57:15 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -200,13 +200,12 @@ export const getCelebrations = async (req, res, next) => {
         },
       },
     }
-    const filter = { $match: { daysTillBirthday: { $gt: 0 } } }
+    // const filter = { $match: { daysTillBirthday: { $gt: 0 } } }
     const sort = { $sort: { daysTillBirthday: 1 } }
     const limit = { $limit: 5 }
     const celebrations = await Users.aggregate([
       pipe1,
       pipe2,
-      filter,
       sort,
       limit,
     ])
